fd1c7c16a30cfcb19e84e6c42c1dc6ff2ddbb67a
bandiasek <ondrejbrendza@gmail.com>
2020-09-06 20:02:56 +0200

    Basic React App

---
+++ README.old.md
@@ -0,0 +1,2 @@
+# WhatsUpClone
+https://www.youtube.com/watch?v=pUxrDcITyjg&amp;list=WL&amp;index=106
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+      apiKey: "AIzaSyBHgTHpojHUjfbwfDE27mLD7pOBKIbVJOs",
+      authDomain: "whatsup-app-clone.firebaseapp.com",
+      databaseURL: "https://whatsup-app-clone.firebaseio.com",
+      projectId: "whatsup-app-clone",
+      storageBucket: "whatsup-app-clone.appspot.com",
+      messagingSenderId: "1030445271650",
+      appId: "1:1030445271650:web:98e79d1b3a8f40f9635f3b",
+      measurementId: "G-S7CDL7L5CB"
+    };
