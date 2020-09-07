19d01ef4b1a8b8a4ef1dc0fd64bbd1efcd336ac8
bandiasek <ondrejbrendza@gmail.com>
2020-09-07 21:54:17 +0200

    Finished next few minutes of tutorial

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import Sidebar from './components/Sidebar';
 
 
 function App() {
@@ -7,7 +8,8 @@ function App() {
     <div className="app">
 
       <div className="app__body">
-
+        {<Sidebar/>}
+        {/* Chat */}
       </div>
     </div>
   );
