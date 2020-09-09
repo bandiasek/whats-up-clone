981d2bcc14758c2336c6f729b1b059e9fd31c4d6
bandiasek <ondrejbrendza@gmail.com>
2020-09-09 20:24:45 +0200

    New stuff done, avatars are cool

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -5,7 +5,7 @@ import DonutLargeIcon from '@material-ui/icons/DonutLarge'
 import ChatIcon from '@material-ui/icons/Chat'
 import MoreVertIcon from '@material-ui/icons/MoreVert'
 import SearchOutlined from '@material-ui/icons/SearchOutlined'
-
+import SidebarChat from './SidebarChat'
 
 function Sidebar() {
       return (
@@ -40,6 +40,11 @@ function Sidebar() {
                   </div>
 
                   <div className="sidebar__chats">
+                        <SidebarChat />
+                        <SidebarChat />
+                        <SidebarChat />
+                        <SidebarChat />
+                        <SidebarChat />
                         
                   </div>
 
